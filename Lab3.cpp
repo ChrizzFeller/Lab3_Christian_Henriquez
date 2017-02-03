5e10238c2151732570e2d2efc201adb2eae913be
ChrizzFeller <chrizzhenriquez@unitec.edu>
2017-02-03 17:58:58 -0600

    Empese y estoy trabajando el ejercicio 2

--- Lab3.cpp
+++ Lab3.cpp
@@ -9,51 +9,76 @@ void imprimir(int**, int);
 int main() {
         int resp = menu();
         if (resp == 1) {
-                int GR, cont, temp, a;
+                int GR, cont, temp = 0, a;
 		cout << "Ingrese el grado mas alto del polinomio. \n";
 		cin >> GR;
 		while (GR <= 0) {
 			cout << "Numero invalido, ingrese otro numero.\n";
 			cin >> GR;
 		}
-		cont = GR;
+		GR++;
+		cont = GR - 1;
 		cout << "Ingrese a.\n";
 		cin >> a;
 		vector<int**> MaT;
 		int** matriz = new int*[3];
 		for(int i = 0; i < 3; i++) {
-			matriz[i] = new int[GR] ;
+			matriz[i] = new int[GR];
+			
 		}
 		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < GR + 1; j++) {
+			for(int j = 0; j < GR; j++) {
 				matriz[i][j] = 0;
+				cout << "[" << matriz[i][j] << "]";			
 			}
+			cout << endl;
 		}
-		for(int i = 0; i < 1; i++) {
-                        for(int j = 0; j < GR + 1; j++) {
+		//for(int i = 0; i < 1; i++) {
+                        for(int j = 0; j < GR; j++) {
 				cout << "Ingrese el polinomio x^" << cont << ".\n";
-				cont--;
 				cin >> temp;
-				matriz[i][j] = temp;
+				cout << j << endl;
+				matriz[0][j] = temp;
+				cout << "P";
+				cont--;
 			}
-		}
-		for(int k = 0; k < GR + 1; k++) {
+		//}
+		cout << "aqui esta el error" << endl;
+		for(int k = 0; k < GR; k++) {
 			for(int i = 0; i < 3; i++) {
-                        	for(int j = 0; j < GR + 1; j++) {
+                        	for(int j = 0; j < GR; j++) {
 					if (i == 1 && matriz[i][j] == 0) {
 						matriz[i+1][j] = matriz[i-1][j];
-					} else {
+					} else if (i == 1) {
 						matriz[i+1][j] = matriz[i-1][j] + matriz[i][j];
 					}
-					if (j != GR + 1) {
+					if (i == 1 && j < GR) {
 						matriz[i][j + 1] = matriz[i+1][j] * a;
 					}
 				}
+			//imprimir(matriz, GR);
+			//cout << endl;
 			}
-			imprimir(matriz, GR+1);
+			imprimir(matriz, GR);
+			cout << endl;
+			MaT.push_back(matriz);
+			//imprimir(matriz, GR);	
 		}
+		for(int i = 0; i < 3; i++) {
+                	delete[] matriz[i];
+                	matriz[i] = NULL;
+        	}
+        	delete[] matriz;
         } else {
-	
+		int tam, minas, x, y;
+		cout << "Ingrese el tamaño de la matriz. \n";
+		cin >> tam;
+		cout << "Ingrese la cantidad de minas. \n";
+		cin >> minas;
+		int** matriz = new int*[tam];
+		for(int i = 0; i < tam; i++) {
+			matriz = new int[tam];
+		}
         }
         return 0;
 }
